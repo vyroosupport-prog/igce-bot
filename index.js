052178319bbd1fd222159264dbeb39eb0d7182db
Light <lightokolie6@gmail.com>
2026-07-04 11:32:49 +0100

    Install Chrome via system package manager

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const client = new Client({
     }),
     puppeteer: {
         headless: true,
+        executablePath: '/usr/bin/google-chrome-stable',
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
