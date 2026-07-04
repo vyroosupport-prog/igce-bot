e8527af0dd3c194499e6e27245bf105e62a64b85
Light <lightokolie6@gmail.com>
2026-07-04 12:38:43 +0100

    Add Render build script

---
+++ .render-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Chrome dependencies and Chrome itself
+apt-get update && apt-get install -y \
+    wget \
+    gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update && apt-get install -y google-chrome-stable --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install Node dependencies
+npm install
